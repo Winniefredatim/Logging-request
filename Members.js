7e0acbe574f781578fd7c341a2cfd2e70b178411
Atim Winniefred <winniefredatimplexa@gmail.com>
2019-11-11 10:53:02 -0800

    first commit

---
+++ Members.js
@@ -0,0 +1,17 @@
+const members = [
+    {
+        id: 1,
+        name: 'Winnie Atim',
+        email: 'winnieatim1996@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Chandiru Carol',
+        email: 'alulcarol@gmail.com',
+        status: 'active'
+    }
+];
+
+
+module.exports = members;
